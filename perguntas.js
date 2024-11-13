8a6d20a1f37a426929d92ebc2c7b9dd2ebc1e3c0
JoãoAlcazas <alcazasjoao@gmail.com>
2024-11-13 15:29:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 1+1?',
+    'Acertou mizerávi'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Qual o país que mais consome café?',
+    'Finlândia'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Qual foi o primeiro console de videogame?',
+    'Magnavox Odyssey'
+)
